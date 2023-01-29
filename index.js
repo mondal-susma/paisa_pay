4d2ed444fad1dc31c8857df0277ee3a448bd7cf0
Susma mondal R <107393298+mondal-susma@users.noreply.github.com>
2023-01-29 12:29:35 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,50 @@
+`use strict`
+var datetime = new Date();
+console.log(datetime);
+document.getElementById("time").textContent = datetime; //it will print on html page
+
+`use strict`;
+function refreshTime() {
+  const timeDisplay = document.getElementById("time");
+  const dateString = new Date().toLocaleString();
+  const formattedString = dateString.replace(", ", " - ");
+  timeDisplay.textContent = formattedString;
+}
+  setInterval(refreshTime, 1000);
+
+  `use strict`
+  var datetime = new Date().getDate();
+  console.log(datetime); // it will represent date in the console of developers tool
+  document.getElementById("time").textContent = datetime; //it will print on html page
+
+  `use strict`
+var datetime = new Date().getDay();
+console.log(datetime); // it will represent date in the console of developers tool
+document.getElementById("time").textContent = datetime; //it will print on html page
+
+`use strict`
+var datetime = new Date().getFullYear();
+console.log(datetime); // it will represent date in the console of developers tool
+document.getElementById("time").textContent = datetime; //it will print on html page
+
+`use strict`
+var datetime = new Date().getHours()+1;
+console.log(datetime); // it will represent date in the console of developers tool
+document.getElementById("time").textContent = datetime; // represent on webbrowser
+
+`use strict`
+var datetime = new Date().getMilliseconds();
+console.log(datetime); // it will represent date in the console of developers tool
+document.getElementById("time").textContent = datetime; // represent on html page
+
+
+`use strict`
+var datetime = new Date().getMonth() + 1;
+console.log(datetime); // it will represent date in the console of  developers tool
+document.getElementById("time").textContent = datetime; // represent on html page
+
+
+`use strict`
+var datetime = new Date().toLocaleTimeString();
+console.log(datetime); // it will represent date in the console of developers tool
+document.getElementById("time").textContent = datetime; // represent on html page
